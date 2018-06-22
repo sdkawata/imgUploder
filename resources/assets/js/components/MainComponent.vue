14cea917986cbb379674376717f5d0586f61cdfb
sdkawata <saddakikawata@gmail.com>
2018-06-22 15:25:09 +0900

    can insert

--- resources/assets/js/components/MainComponent.vue
+++ resources/assets/js/components/MainComponent.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
     <h2>アップロード画像一覧</h2>
+    <h1>画像アップローダー</h1>
+    <h2>画像をアップロードする</h2>
+    画像ファイル<input type="file" ref="file"><br>
+    コメント<input type="text" v-model="comment"><br>
+    <button type="submit" v-on:click="uploadImage">アップロード</button>
     <image-item
     v-for="image in images"
     v-bind:key="image.id"
@@ -11,9 +16,20 @@
   export default {
     data() {
       return {
-        images: []
+        images: [],
+        comment: ''
       }
     },
+  	methods: {
+      uploadImage() {
+      	let formData = new FormData()
+      	formData.append('file', this.$refs.file.files[0])
+      	formData.append('comment', this.comment)
+      	axios.post('/api/', formData).then((response) => {
+        this.images.push(response.data.data)
+      	})
+      }
+  	},
     mounted() {
       // コンポーネントが読み込まれるときに実行される。
       // APIからデータを取得してデータに入れる
